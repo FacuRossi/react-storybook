e166ce1a45acac6ee82f05850c0bf3a43630ea86
Facu Rossi <rossifacundo94@gmail.com>
2018-12-28 17:06:42 -0300

    Add TaskList and some storybooks

---
+++ src/components/Task.stories.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+import Task from './Task';
+
+export const task = {
+    id: '1',
+    title: 'Test Task',
+    state: 'TASK_INBOX',
+    updatedAt: new Date(2018, 0, 1, 9, 0),
+};
+
+export const actions = {
+    onPinTask: action('onPinTask'),
+    onArchiveTask: action('onArchiveTask'),
+};
+
+//{...actions} == <Task onPinTask={actions.onPinTask} onArchiveTask={actions.onArchiveTask}>
+storiesOf('Task', module)
+    .add('default', () => <Task task={task} {...actions} />)
+    .add('pinned', () => <Task task={{ ...task, state: 'TASK_PINNED' }} {...actions} />)
+    .add('archived', () => <Task task={{ ...task, state: 'TASK_ARCHIVED' }} {...actions} />);
